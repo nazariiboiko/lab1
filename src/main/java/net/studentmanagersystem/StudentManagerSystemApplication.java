9ef22611c125f5ee981a4743785775715178c87d
Nazarii Boiko <nazidokss1@gmail.com>
2022-12-15 10:49:03 +0200

    cr fldrs

---
+++ src/main/java/net/studentmanagersystem/StudentManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package net.studentmanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/studentmanagersystem/StudentManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.studentmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
